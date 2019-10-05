a40a1a3b2e8ff33cfed047dda478a220014753bf
Kemal YILDIRIM <kemalyldrim@gmail.com>
2019-10-05 18:43:41 +0100

    Add project files.

---
+++ ConsoleApp/Configuration/AutofacSetup.cs
@@ -0,0 +1,42 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using ConsoleApp.Helpers;
+using ConsoleApp.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace ConsoleApp.Configuration
+{
+    public static class AutofacSetup
+    {
+        internal static IServiceProvider RegisterServices(IServiceProvider serviceProvider)
+        {
+            var collection = new ServiceCollection();
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<AmbientTemperatureProcessor>().As<IAmbientTemperatureProcessor>();
+            builder.RegisterType<TyreTemperatureProcessor>().As<ITyreTemperatureProcessor>();
+            builder.RegisterType<FileReader>().As<IFileReader>();
+            builder.RegisterType<CalculateTempratures>().As<ICalculateTempratures>();
+
+            builder.Populate(collection);
+
+            var appContainer = builder.Build();
+
+            serviceProvider = new AutofacServiceProvider(appContainer);
+            return serviceProvider;
+        }
+
+        internal static void DisposeServices(IServiceProvider serviceProvider)
+        {
+            if (serviceProvider == null)
+            {
+                return;
+            }
+            if (serviceProvider is IDisposable)
+            {
+                ((IDisposable)serviceProvider).Dispose();
+            }
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        private static IServiceProvider _serviceProvider;
+
+        static void Main(string[] args)
+        {
+            _serviceProvider = AutofacSetup.RegisterServices(_serviceProvider);
+
+            var calculateTempratures = _serviceProvider.GetService<ICalculateTempratures>();
+
+            try
+            {
+                var result = calculateTempratures.Execute();
+            }
+            catch (Exception e)
+            {
+                // todo: It should be written in a log file and keep continue to program based on exception type.
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine(e.Message.ToString());
+                Console.WriteLine("Please restart the program.");
+            }
+
+            Console.ReadKey();
+            AutofacSetup.DisposeServices(_serviceProvider);
+        }
+    }
+}
